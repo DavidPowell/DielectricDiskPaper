299421525596be10cec367db4a8899df29e6634a
David Powell <david.a.powell@anu.edu.au>
2017-02-03 13:27:05 +1100

    Updated all calculations for holey disks and elliptical cylinders. Automated VTk generation of 3D png plots. Updated all notebooks to generate figures.

---
+++ paraview_disk.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Use Paraview to generate the .png bitmap image files of modes from 3d .vtp files
+
+Paraview must be installed, and the script should be run with the `pvpython`
+Python interpreter embedded into Paraview.
+
+@author: dap124
+"""
+
+from paraview.simple import *
+import sys
+
+#input_filenames = ["disk-modes-{:02d}.vtp".format(n) for n in range(7)]
+#output_filenames = ["disk-modes-{:02d}.png".format(n) for n in range(7)]
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+reader = XMLPolyDataReader(FileName=input_file)
+
+view = servermanager.CreateRenderView()
+ 
+
+# set the camera position and angle
+view.CameraViewUp = [0, 0, 1]
+view.CameraFocalPoint = [0, 0, 0]
+view.CameraPosition = [3.3,0,2.5]
+view.OrientationAxesVisibility = 0
+zoom_level = 1.55
+
+#set image size
+view.ViewSize = [800, 700] #[width, height] 
+
+# allow the plotted quantity to be specified on command line
+if len(sys.argv) > 4:
+    scalar_name = sys.argv[3]
+    vector_name = sys.argv[4]
+else:
+    scalar_name = "rho_e_imag"
+    vector_name = "J_imag"
+
+#set the background color
+view.Background = [1,1,1]  # white
+
+repr = servermanager.CreateRepresentation(reader, view)
+repr.Representation = "Surface"
+reader.UpdatePipeline()
+
+dataInfo = reader.GetDataInformation()
+
+# The scalar surface charges are represented as cell data
+cellDataInfo = dataInfo.GetCellDataInformation()
+chargeInfo = cellDataInfo.GetArrayInformation(scalar_name)
+
+range = chargeInfo.GetComponentRange(0)
+lut = servermanager.rendering.PVLookupTable()
+                  
+lut.RGBPoints  = [range[0], 0.231373, 0.298039, 0.752941,
+                  range[1], 0.705882, 0.156863, 0.14902]
+
+repr.LookupTable = lut
+repr.ColorArrayName = ['CELLS', scalar_name]
+repr.UpdatePipeline()
+
+currentInfo = cellDataInfo.GetArrayInformation(vector_name)
+ 
+gl = Glyph()
+gl.Vectors = ['CELLS', vector_name]
+gl.GlyphType = 'Cone'
+gl.ScaleFactor = 2e-8
+ 
+repr = servermanager.CreateRepresentation(gl, view)
+gl.UpdatePipeline()
+
+# zooming must be done after an initial render has worked out automatic camera placement
+Render(view)
+cam = view.GetActiveCamera()
+cam.Zoom(zoom_level)
+
+
+Render(view)
+SaveScreenshot(output_file, view, magnification=2)
---
+++ runpvpy.bat
@@ -0,0 +1,18 @@
+"C:\Program Files\ParaView 5.1.2\bin\pvpython.exe" paraview_disk.py disk-modes-00.vtp disk-modes-00.png
+"C:\Program Files\ParaView 5.1.2\bin\pvpython.exe" paraview_disk.py disk-modes-01.vtp disk-modes-01.png
+"C:\Program Files\ParaView 5.1.2\bin\pvpython.exe" paraview_disk.py disk-modes-02.vtp disk-modes-02.png
+"C:\Program Files\ParaView 5.1.2\bin\pvpython.exe" paraview_disk.py disk-modes-03.vtp disk-modes-03.png
+"C:\Program Files\ParaView 5.1.2\bin\pvpython.exe" paraview_disk.py disk-modes-04.vtp disk-modes-04.png
+"C:\Program Files\ParaView 5.1.2\bin\pvpython.exe" paraview_disk.py disk-modes-05.vtp disk-modes-05.png
+"C:\Program Files\ParaView 5.1.2\bin\pvpython.exe" paraview_disk.py disk-modes-06.vtp disk-modes-06.png
+
+"C:\Program Files\ParaView 5.1.2\bin\pvpython.exe" paraview_disk.py holey-disk-modes-00.vtp holey-disk-modes-00.png
+"C:\Program Files\ParaView 5.1.2\bin\pvpython.exe" paraview_disk.py holey-disk-modes-02.vtp holey-disk-modes-02.png
+"C:\Program Files\ParaView 5.1.2\bin\pvpython.exe" paraview_disk.py holey-disk-modes-01.vtp holey-disk-modes-01.png
+
+"C:\Program Files\ParaView 5.1.2\bin\pvpython.exe" paraview_elliptical_cylinder.py elliptical-cylinder-modes-00.vtp elliptical-cylinder-modes-00.png
+"C:\Program Files\ParaView 5.1.2\bin\pvpython.exe" paraview_elliptical_cylinder.py elliptical-cylinder-modes-01.vtp elliptical-cylinder-modes-01.png
+"C:\Program Files\ParaView 5.1.2\bin\pvpython.exe" paraview_elliptical_cylinder.py elliptical-cylinder-modes-02.vtp elliptical-cylinder-modes-02.png
+"C:\Program Files\ParaView 5.1.2\bin\pvpython.exe" paraview_elliptical_cylinder.py elliptical-cylinder-modes-03.vtp elliptical-cylinder-modes-03.png
+"C:\Program Files\ParaView 5.1.2\bin\pvpython.exe" paraview_elliptical_cylinder.py elliptical-cylinder-modes-04.vtp elliptical-cylinder-modes-04.png rho_e_real J_real
+"C:\Program Files\ParaView 5.1.2\bin\pvpython.exe" paraview_elliptical_cylinder.py elliptical-cylinder-modes-05.vtp elliptical-cylinder-modes-05.png
